22c2c0b1d986b5944b1ff551b8b3ab5870a9b757
Jiawei Zhao <Phoenxi500526@163.com>
2022-11-02 13:56:41 +0800

    update rustlings tasks

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -10,13 +10,19 @@
 // No boiler plate code this time,
 // you can do this!
 
-// I AM NOT DONE
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 //TODO: Add your code here
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut buf = self.clone();
+        buf.push(String::from("Bar"));
+        buf
+    }
+}
 
 #[cfg(test)]
 mod tests {
